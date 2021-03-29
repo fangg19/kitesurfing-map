9631f875c453eecf7ba26ebc34e391b39ff7b003
Șerban Alexandru Jonny <alexjohnny6@gmail.com>
2021-03-29 21:51:24 +0300

    Login component with no func

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import classes from './Login.module.css';
-import { NavLink } from 'react-router-dom';
 
-const Login = () => {
+const Login = ({ history }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    axios
+      .post('https://605ce5a96d85de00170db441.mockapi.io/spot', {
+        username,
+        password,
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  };
   return (
     <div className={classes.Login}>
-      <p>
-        Go to <NavLink to="/dashboard">Dashboard.</NavLink>
-      </p>
+      <h1>Kite</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.FormControl}>
+          <label htmlFor="username">Username: </label>
+          <input
+            type="text"
+            name="username"
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+        </div>
+        <div className={classes.FormControl}>
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            name="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        <input type="submit" value="Login" />
+      </form>
     </div>
   );
 };
